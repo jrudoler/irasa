7038f502020f79d905a7708e4cf35cb2756e7e11
Joseph Rudoler <40604402+jrudoler@users.noreply.github.com>
2022-03-29 17:03:38 -0400

    Update setup.py
    Add package config

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
+
 setup(name='irasa',
       author="Joseph Rudoler",
+      packages=find_packages(),
       version='0.1',
       py_modules=['irasa'],
       )
